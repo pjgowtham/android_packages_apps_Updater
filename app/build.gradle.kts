c92dd91e29c68f2898d7802963922c85512376ef
pjgowtham <pjgowtham@gmail.com>
2026-03-20 23:13:37 +0530

    Updater: Drop material library dependency
    Change-Id: Id51435c246fcad8954bc401896883f4be2e5f7ff

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -120,7 +120,6 @@ dependencies {
     implementation(libs.ktor.client.content.negotiation)
     implementation(libs.ktor.client.core)
     implementation(libs.ktor.serialization.kotlinx.json)
-    implementation(libs.material)
 
     annotationProcessor(libs.androidx.room.compiler)
 }
