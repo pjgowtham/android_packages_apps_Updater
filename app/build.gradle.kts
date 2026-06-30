72389f8f61fe526558436a0f4167787c592e9ed6
pjgowtham <pjgowtham@gmail.com>
2026-06-30 17:44:49 +0530

    Updater: Add Leanback dependencies
    Change-Id: Ie919ddf6f7cb7fd5c0d17f65f5fbdf31324b4d95

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -103,6 +103,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.datastore.preferences)
+    implementation(libs.androidx.leanback)
+    implementation(libs.androidx.leanback.preference)
     implementation(libs.androidx.lifecycle.livedata.ktx)
     implementation(libs.androidx.lifecycle.runtime.compose)
     implementation(libs.androidx.lifecycle.viewmodel.ktx)
